7b5cab458276a7f2acba456af9262687f3705aa8
Jai Verma <jai2.verma@outlook.com>
2022-11-22 19:05:21 +0000

    Adding function to get Spotify auth token

--- bin/main.ml
+++ bin/main.ml
@@ -2,20 +2,28 @@ open Lwt
 open Cohttp
 open Cohttp_lwt_unix
 
-let body =
-  Client.get (Uri.of_string "https://www.reddit.com/")
+let auth_and_get_token () =
+  let url = Uri.of_string "https://accounts.spotify.com/api/token" in
+  let client_id = Sys.getenv "CLIENT_ID" in
+  let client_secret = Sys.getenv "CLIENT_SECRET" in
+  let token = Base64.encode_exn @@ client_id ^ ":" ^ client_secret in
+  let headers =
+    Header.of_list
+      [ "Authorization", "Basic " ^ token
+      ; "Content-Type", "application/x-www-form-urlencoded"
+      ]
+  in
+  let body =
+    Cohttp_lwt.Body.of_string
+    @@ Uri.encoded_of_query [ "grant_type", [ "client_credentials" ] ]
+  in
+  Client.post ~body ~headers url
   >>= fun (resp, body) ->
-  let code = resp |> Response.status |> Code.code_of_status in
-  Printf.printf "Response code: %d\n" code;
-  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
-  body
-  |> Cohttp_lwt.Body.to_string
-  >|= fun body ->
-  Printf.printf "Body of length: %d\n" (String.length body);
-  body
+  Printf.printf "Response code: %d\n" (resp |> Response.status |> Code.code_of_status);
+  Cohttp_lwt.Body.to_string body
 ;;
 
 let () =
-  let _body = Lwt_main.run body in
-  print_endline "Hello world"
+  let body = Lwt_main.run @@ auth_and_get_token () in
+  print_endline @@ body ^ "\n"
 ;;
