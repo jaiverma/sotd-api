29bdd878f7c03710a070da2ceda973cf5fcd83c3
Jai Verma <jai2.verma@outlook.com>
2023-07-14 11:20:23 +0100

    Adding hacky logging to notes_client

--- notes_client.py
+++ notes_client.py
@@ -3,6 +3,12 @@
 import socket
 import struct
 import sys
+import logging
+
+logging.basicConfig(filename='/tmp/notes_client.log',
+                    filemode='a',
+                    level=logging.INFO)
+logger = logging.getLogger('notes_client')
 
 socket_path = '/tmp/notes_state.sock'
 
@@ -10,7 +16,7 @@ s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 try:
     s.connect(socket_path)
 except socket.error as msg:
-    print(msg)
+    logger.info(msg)
     sys.exit(0)
 
 msg = b''
@@ -19,3 +25,5 @@ msg += struct.pack('<I', 1)
 
 s.send(msg)
 s.close()
+
+logger.info('[+] Update success')
