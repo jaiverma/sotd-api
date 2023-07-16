7f634c0e64726073b3d5ef1197380dc9e6f88f47
Jai Verma <jai2.verma@outlook.com>
2023-07-16 23:40:45 +0100

    Removing unnecessary socket based save_state code

--- notes.py
+++ notes.py
@@ -3,10 +3,6 @@ import pickle
 import time
 import pytz
 import os
-import threading
-import select
-import socket
-import struct
 from uwsgidecorators import *
 from datetime import datetime, timedelta
 
@@ -56,18 +52,20 @@ def load_state(config_path):
         random.seed(time.time())
         state = random.getstate()
         with open(RANDOM_STATE_PATH, 'wb') as f:
+            print('writing state')
             pickle.dump(state, f)
 
     return state
 
 @cron(-1, -1, -1, -1, -1)
-def save_state(state=random.getstate()):
+def save_state(_num=0):
     global HI_NOTE_IDX
     global LOVE_NOTE_IDX
 
     print('[*] save_state called')
 
     with open(RANDOM_STATE_PATH, 'wb') as f:
+        state = random.getstate()
         pickle.dump(state, f)
 
     new_hi_note_idx = random.randint(0, len(HI_NOTES) - 1)
@@ -80,60 +78,6 @@ def save_state(state=random.getstate()):
         new_love_note_idx = random.randint(0, len(LOVE_NOTES) - 1)
     LOVE_NOTE_IDX = new_love_note_idx
 
-# handle client data
-# - [0:4] : c0 c0 00 00
-# - [5]   : 01
-# - [6:8] : 00 00 00
-def handle_client_msg(msg):
-    if len(msg) != 8:
-        return False
-
-    magic = struct.unpack('<I', msg[:4])[0]
-    if magic != 0xc0c0:
-        return False
-
-    toggle = struct.unpack('<I', msg[4:])[0]
-    if toggle != 1:
-        return False
-
-    save_state()
-    return True
-
-def state_listener():
-    server = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    socket_path = '/tmp/notes_state.sock'
-
-    # clean up existing socket if it may exist
-    try:
-        os.unlink(socket_path)
-    except OSError:
-        if os.path.exists(socket_path):
-            raise
-
-    server.setblocking(0)
-    server.bind(socket_path)
-    server.listen(5)
-
-    inputs = [server]
-
-    while True:
-        readable, _, err = select.select(inputs, [], inputs, 0.5)
-        for s in readable:
-            if s is server:
-                connection, client_info = s.accept()
-                connection.setblocking(0)
-                inputs.append(connection)
-            else:
-                # this is a connected client
-                data = s.recv(1024)
-                handle_client_msg(data)
-                inputs.remove(s)
-                s.close()
-
-        for s in err:
-            inputs.remove(s)
-            s.close()
-
 def init():
     state = load_state('./conf.txt')
     save_state()
@@ -143,9 +87,6 @@ def init():
     except TypeError:
         print('[-] Failed to load random state')
         pass
-
-    # t = threading.Thread(target=state_listener)
-    # t.start()
 
 def get_hi_note():
     return 'Hu ' + HI_NOTES[HI_NOTE_IDX]
